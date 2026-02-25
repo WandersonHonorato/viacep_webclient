ba840f6a2b76c9244b1eff94cff3fcdebe8f1991
Wanderson <faturamento@novaterraambiental.srv.br>
2026-02-25 11:13:27 -0300

    initial commit

---
+++ src/test/java/viacep/webclient/ViacepApplicationTests.java
@@ -0,0 +1,13 @@
+package viacep.webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViacepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
