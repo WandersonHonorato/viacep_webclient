067738d02cf512f8c1ea7c1960411cc3331658b7
Wanderson <faturamento@novaterraambiental.srv.br>
2026-02-26 14:17:41 -0300

    feat: corrige retorno reativo usando Mono.map(ResponseEntity::ok)

--- src/main/java/viacep/webclient/controller/CepController.java
+++ src/main/java/viacep/webclient/controller/CepController.java
@@ -1,4 +1,24 @@
 package viacep.webclient.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+import viacep.webclient.dto.ViaCepResponse;
+import viacep.webclient.service.ViaCepService;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/cep")
 public class CepController {
+
+   private final ViaCepService viaCepService;
+
+
+   @GetMapping("/{cep}")
+   public Mono<ResponseEntity<ViaCepResponse>> buscar(@PathVariable String cep){
+      return viaCepService.buscarCep(cep)
+         .map(ResponseEntity::ok);
+   }
 }
