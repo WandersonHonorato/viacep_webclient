b073ddb5368e1a6a806aa0592ac65c593b38945b
Wanderson <faturamento@novaterraambiental.srv.br>
2026-03-04 10:27:25 -0300

    feat: criar record ErrorResponse para retornar o erro do endpoint invés do Map

---
+++ src/main/java/viacep/webclient/response/ErrorResponse.java
@@ -0,0 +1,12 @@
+package viacep.webclient.response;
+
+import java.time.LocalDateTime;
+
+public record ErrorResponse(
+        LocalDateTime timestamp,
+        int status,
+        String error,
+        String message,
+        String path
+) {
+}
